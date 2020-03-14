c99da6381c94293c52cd7b228ab38c5cdadd6241
Hamza Rafique <hamzarafique964@gmail>
2020-03-14 15:23:43 -0700

    Updates

---
+++ precache-manifest.a593c3127ae6b18bd6ae2c42552df74d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58dfb985b4d8699d38b04d3048950fe0",
+    "url": "/Routing-To-do-App-MultiForm/index.html"
+  },
+  {
+    "revision": "506ef2176018fd772031",
+    "url": "/Routing-To-do-App-MultiForm/static/css/main.3272ed97.chunk.css"
+  },
+  {
+    "revision": "2b27cebb78ca3ae66cda",
+    "url": "/Routing-To-do-App-MultiForm/static/js/2.8038c617.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Routing-To-do-App-MultiForm/static/js/2.8038c617.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "506ef2176018fd772031",
+    "url": "/Routing-To-do-App-MultiForm/static/js/main.ff536f76.chunk.js"
+  },
+  {
+    "revision": "e8d9c1a7f3d759bb6d59",
+    "url": "/Routing-To-do-App-MultiForm/static/js/runtime-main.005cad2a.js"
+  }
+]);
